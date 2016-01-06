0f18a3f72368a19b784cbf29279c3a83447c2cd8
Wray Bowling <wray@richlandlibrary.com>
2016-01-06 10:27:04 -0500

    session defaults are in their own file

--- client/main.js
+++
@@ -1,9 +0,0 @@
-if(Session.get('date') === undefined){
-	Session.set('date',moment().date());
-}
-
-if(Session.get('filters') === undefined){
-	Session.set('filters',{
-		iso_end: {$gte:moment()._d, $lte:moment().endOf('day')._d}
-	});
-}
