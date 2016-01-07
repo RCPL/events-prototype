cc513ff17ee2b2581442e5983a44677aff2e9d96
Wray Bowling <wray@richlandlibrary.com>
2016-01-07 10:54:33 -0500

    BROKEN: single pages aren't helped out by publications...?

--- server/publications/events.js
+++ server/publications/events.js
@@ -3,3 +3,7 @@ Meteor.publish('events', function() {
 	var eow = moment(new Date()).endOf("day").add(30,'days');
 	return Events.find({iso_end: {$gte: now, $lte: eow._d}},{sort:{iso_start: 1},limit:400});
 });
+
+Meteor.publish('singleEvent', function(id) {
+	return Events.findOne({id: id});
+});
